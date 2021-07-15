4fa1abba6bf642f5a5015b0e1d0d44fbc3513be1
EC2 Default User <ec2-user@ip-172-31-23-174.ec2.internal>
2021-07-15 11:13:54 +0000

    アプリケーションを完成させよう

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,57 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+  
+  def show
+    @book = Book.find(params[:id])
+  end
+
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+   @book = Book.new(book_params)
+   if @book.save
+   flash[:notice] = "Book was successfully created." 
+   redirect_to book_path(@book)
+   else
+    @books = Book.all
+    flash[:notice2] = "2 errors prohibited this book from being saved:" 
+    render action: :index
+   end
+     
+  end
+  
+  def edit
+    @book = Book.find(params[:id])
+  
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    flash[:notice] = 'Book was successfully updated.'
+    redirect_to book_path(@book)
+    else
+    flash[:notice2] = "2 errors prohibited this book from being saved:" 
+    render action: :edit
+    end
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    flash[:notice] = "Book was successfully destroyed."
+    redirect_to books_path
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
